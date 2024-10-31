79fd35984f5e0a8e37f8b25018fc15558de5124c
Anwesa Ghosh <anwesa2021@gmail.com>
2024-11-01 01:29:45 +0530

    hello1

--- mySpotify_clone/src/components/Sidebar.jsx
+++ mySpotify_clone/src/components/Sidebar.jsx
@@ -29,8 +29,13 @@ const Sidebar = () => {
                 <h1>Create your first playlist</h1>
                 <p className='font-light'> It's easy we will help you</p>
                 <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
-
             </div>
+            <div className='p-4 bg-[#242424] m-2 rounded flex-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                <h1>Let's find some podcasts to follow</h1>
+                <p className='font-light'> We'll keep you update on new episodes</p>
+                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcasts</button>
+            </div>
+            
         </div>
 
     </div>
